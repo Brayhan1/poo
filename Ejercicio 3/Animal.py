b6a6bf8d85b4f9fc0d1e0a05b04b190329339f8b
Brayhan1 <al24760261@ite.edu.mx>
2026-03-08 00:53:18 -0800

    Add files via upload

---
+++ Ejercicio 3/Animal.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(yo, nombre, edad)
+        yo.nombre=nombre
+        yo.edad=edad
+
+    def describir (yo)
+        print (f "soy {yo.nombre} y tengo {yo.edad} años")
+
+    def hablar (yo)
+        print (".....")
---
+++ Ejercicio 3/Perro.py
@@ -0,0 +1,5 @@
+from Animal import Animal
+class Perro (Animal):
+    def hablar(yo):
+        print (f"{yo.nombre}: !GUAUUU")
+        
